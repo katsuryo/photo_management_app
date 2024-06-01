ad251a2202dda86450f01ec563306bf821fe7bb1
katsuryo <katsuryo68@gmail.com>
2024-06-01 16:54:13 +0900

    [feat]認証処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def authenticate_user
+    @current_user ||= User.find_by(id: session[:user_id])
+    
+    unless @current_user
+      redirect_to root_path, alert: 'ログインしてください'
+    end
+  end
+
 end
