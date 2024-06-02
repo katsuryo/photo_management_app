24c31f36a364d888400508f0ecb923e3d7d049bc
katsuryo <katsuryo68@gmail.com>
2024-06-02 18:14:44 +0900

    [feat]ログイン状態でトップページに遷移した場合、写真一覧画面へ遷移させる

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def new
-  #  loginのview
+    user = User.find_by(id: session[:user_id])
+    redirect_to photos_path if user
   end
 
   def create
